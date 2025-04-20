8e52ab80b0be4179b839104a4e84898e9408ab2f
IlyaBurlak <113059204+IlyaBurlak@users.noreply.github.com>
2025-04-20 20:46:34 +0300

    ашч: build

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import {Analytics} from "@vercel/analytics/vue";
-import {SpeedInsights} from "@vercel/speed-insights/vue";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <App />
         <Analytics />
         <SpeedInsights />
-    </StrictMode>,
+    </StrictMode>
 );
