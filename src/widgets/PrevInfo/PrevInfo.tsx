eeada023790298700ac23c417dd1a15627772594
IlyaBurlak <113059204+IlyaBurlak@users.noreply.github.com>
2025-06-06 21:31:44 +0300

    refactor: add link CV

--- src/widgets/PrevInfo/PrevInfo.tsx
+++ src/widgets/PrevInfo/PrevInfo.tsx
@@ -1,4 +1,15 @@
-function PrevInfo(){
+function PrevInfo() {
+    const handleDownloadCV = () => {
+        const cvUrl = 'public/CV.pdf';
+
+        const link = document.createElement('a');
+        link.href = cvUrl;
+        link.download = 'Ilya_Frontend_Developer_CV.pdf';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
     return (
         <header className="header">
             <div className="header__wrapper">
@@ -9,7 +20,7 @@ function PrevInfo(){
                 <div className="header__text">
                     <p>with passion for learning and creating.</p>
                 </div>
-                <a id='CV-btn' className="btn">Download CV</a>
+                <a id='CV-btn' className="btn" onClick={handleDownloadCV}>Download CV</a>
             </div>
         </header>
     )
